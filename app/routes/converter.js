4dad74bfafb481333be9d33c5e8024886cc024c4
Phnc <paulohn27@hotmail.com>
2022-02-18 11:14:51 -0300

    add try catch

--- app/routes/converter.js
+++ app/routes/converter.js
@@ -2,35 +2,37 @@ const puppeteer = require('puppeteer');
 
 module.exports = function(app){
     app.post("/api/Html/convert/pdf", async (req, res) => {
-        const id = req.body.id;
-        let htmlString = req.body.Content;
-        htmlString = htmlString.replace("http://sir.", "http://wssir.");
-        htmlString = htmlString.replace("http://sir.", "http://wssir.");
-
-        console.log(htmlString);
-
-        const browser = await puppeteer.launch({
-            args: ['--headless', '--disable-gpu', '--full-memory-crash-report', '--unlimited-storage',
-                   '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
-        });
-
-        const page = await browser.newPage();
-
-        await page.setDefaultNavigationTimeout(0); 
-
-        await page.setContent(htmlString);
-
-        const buffer = await page.pdf({ format: "A4" });
-
-        await browser.close();
-
-        const base64 = buffer.toString('base64');
-
-        const result = {
-            id: id,
-            content: base64
+        try{
+            const id = req.body.id;
+            let htmlString = req.body.Content;
+            htmlString = htmlString.replace("http://sir.", "http://wssir.");
+            htmlString = htmlString.replace("http://sir.", "http://wssir.");
+    
+            const browser = await puppeteer.launch({
+                args: ['--headless', '--disable-gpu', '--full-memory-crash-report', '--unlimited-storage',
+                       '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+            });
+    
+            const page = await browser.newPage();
+    
+            await page.setDefaultNavigationTimeout(0); 
+    
+            await page.setContent(htmlString);
+    
+            const buffer = await page.pdf({ format: "A4" });
+    
+            await browser.close();
+    
+            const base64 = buffer.toString('base64');
+    
+            const result = {
+                id: id,
+                content: base64
+            }
+            
+            res.status(200).json(result);
+        }catch(e){
+            console.log(e);
         }
-        
-        res.status(200).json(result);
     });
 }
